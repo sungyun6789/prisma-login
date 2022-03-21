a35d200069cfc6362481ff6a0dbf73df8fd9284f
sungyun <sungyun5423@gmail.com>
2022-03-21 23:40:32 +0900

    feat: index page routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
+
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 const Index: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/signin');
+  }, []);
+
   return <>Home</>;
 };
 
