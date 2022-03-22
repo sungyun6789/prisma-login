38388df474ffeb5ef4c1d3ae04eb8084afe29236
sungyun <sungyun5423@gmail.com>
2022-03-22 23:33:18 +0900

    style: main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from 'react';
 
+import styled from '@emotion/styled';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
@@ -13,7 +14,41 @@ const Index: NextPage = () => {
     user ?? router.push('/signin');
   }, []);
 
-  return <>Home</>;
+  return (
+    <Main>
+      <div>
+        <h1>안녕하세요. {user?.email}</h1>
+        <LogoutButton>로그아웃</LogoutButton>
+      </div>
+    </Main>
+  );
 };
 
 export default Index;
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  h1 {
+    text-align: center;
+    font-size: 30px;
+    margin-bottom: 70px;
+  }
+`;
+
+const LogoutButton = styled.button`
+  display: block;
+  width: 300px;
+  height: 50px;
+
+  border: 1px solid #e0e0e0;
+  border-radius: 10px;
+
+  background-color: white;
+
+  font-size: 16px;
+  cursor: pointer;
+`;
