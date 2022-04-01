3efeccc2bbdd858d7631ff0138cece528312c8f7
sungyun <sungyun5423@gmail.com>
2022-04-01 20:33:52 +0900

    deploy: production 환경 변수는 배포 환경에서 설정함

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,11 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.NODE_ENV === 'production' ? process.env.PROD_DATABASE_URL : process.env.DATABASE_URL,
-    },
-  },
-});
+const prisma = new PrismaClient();
 
 export default prisma;
