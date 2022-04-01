bd13faa6fd685249a78edad9333d35cd1a1760ba
sungyun <sungyun5423@gmail.com>
2022-04-01 19:47:30 +0900

    deploy: db setting for deploy

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.NODE_ENV === 'production' ? process.env.PROD_DATABASE_URL : process.env.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
